953ffb1310c629abaab1ada9dc002c19cd2bf201
Md Ashraf Uddin <mdtanjil01753511918@gmail.com>
2019-02-21 00:13:56 +0600

    first commit

---
+++ shared/footer.php
@@ -0,0 +1,105 @@
+<section class="portfolioarea text-center">
+			<div class="container">
+				<div class="ourportfolio">
+					<h2>Our Featured Works</h2>
+					<p>Curabitur blandit tempus porttitor. Maecenas faucibus mollis interdum.</p>
+			
+					<div id="filters" class="button-group ">  <button class="button is-checked" data-filter="*">all</button>
+					  <button class="button" data-filter=".metal">graphic</button>
+					  <button class="button" data-filter=".transition">llustration</button>
+					  <button class="button" data-filter=".alkali, .alkaline-earth">motion</button>
+				
+					</div>
+
+
+
+					<div class="grid">
+					  <div class="element-item transition metal " data-category="transition">
+						<img src="img/hom1e.png"/>
+					  </div>
+					  <div class="element-item metalloid " data-category="metalloid">
+					   <img src="img/home.png"/>
+					  </div>
+					  <div class="element-item post-transition metal " data-category="post-transition">
+					<img src="img/home3.png"/>
+					  </div>
+					  <div class="element-item post-transition  " data-category="post-transition">
+						<img src="img/hom1e.png"/>
+					  </div>
+					  <div class="element-item transition metal " data-category="transition">
+					<img src="img/home.png"/>
+					  </div>
+					  <div class="element-item alkali metal " data-category="alkali">
+					<img src="img/home3.png"/>
+					  </div>
+					  <div class="element-item alkali metal " data-category="alkali">
+					<img src="img/home3.png"/>
+					  </div>
+					  <div class="element-item transition metal " data-category="transition">
+						<img src="img/hom1e.png"/>
+					  </div>
+					  <div class="element-item alkaline-earth metal " data-category="alkaline-earth">
+					<img src="img/home.png"/>
+					  </div>
+					  <div class="element-item transition metal " data-category="transition">
+					<img src="img/hom1e.png"/>
+					  </div>
+					  <div class="element-item post-transition metal " data-category="post-transition">
+					<img src="img/home3.png"/>
+					  </div>
+					  <div class="element-item metalloid " data-category="metalloid">
+					<img src="img/home.png"/>
+					  </div>
+					
+
+				</div>
+			</div>
+		</section><!-------end isotip --------->
+		<section class="contactsection text-center">
+			<div class="container">
+				<div class="contact">
+					<h2>Get in Touch</h2>
+					<h4>Vestibulum id ligula porta felis euismod semper,malesuada euismod.</h4>
+					<div class="ouraddress">
+						<p class="address">Moonshine Street No: 14/05 Light City, Jupiter</p>
+						<p class="phone">247 541 65 87</p>
+					</div>
+					<div class="social text-center ">
+					
+						<i class="fa fa-rss rf" aria-hidden="true"></i>
+						<i class="fa fa-facebook bf" aria-hidden="true"></i>
+						<i class="fa fa-tumblr tf" aria-hidden="true"></i>
+						<i class="fa fa-times-circle cf" aria-hidden="true"></i>
+						<i class="fa fa-pinterest-p pf" aria-hidden="true"></i>
+
+
+
+
+					</div>
+				</div>
+			</div>
+		</section><!-------end contactsection --------->
+		<footer class="footersection">
+			<div class="container">
+				<p>&copy; 2013 Webpaint. All Rights Reserved.</p>
+			</div>
+		</footer<!-------end footersection --------->
+        <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+        <script src="js/sotope.pkgd.min.js"></script>
+        <script src="js/isotip.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='https://www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+        </script>
+    </body>
+</html>
---
+++ shared/header.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <!-- Place favicon.ico in the root directory -->
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+		<link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/styles.css">
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/jquery.bxslider.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/reponsive.css">
+		
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+		
+    </head>
